ae609cd23bb3c4f2c9f0344ac9f987e9369a845d
Yildirayakzm <yildirayakzm@users.noreply.github.com>
2026-06-26 22:43:33 +0300

    Remove temporary installation scripts

--- git_kurulum.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Kurulum basliyor... Windows guvenlik uyarisi (Evet/Hayir) cikarsa lutfen EVET e tiklayin.
-winget install --id Git.Git -e --source winget --accept-package-agreements --accept-source-agreements
-echo.
-echo Kurulum tamamlandi! Lutfen bu pencereyi kapatin ve bana haber verin.
-pause
